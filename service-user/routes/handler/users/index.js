c735b38d1e2fae7ca6f6a7adbbda12bb884c8de7
Adi Kurniawan <adikurniawan.dev@gmail.com>
2021-12-29 14:57:32 +0700

    feat(user): create api logout

--- service-user/routes/handler/users/index.js
+++ service-user/routes/handler/users/index.js
@@ -3,6 +3,8 @@ const login = require('./login');
 const update = require('./update');
 const getUser = require('./getUser');
 const getUsers = require('./getUsers');
+const logout = require('./logout');
+
 
 
 module.exports = {
@@ -10,5 +12,6 @@ module.exports = {
     login,
     update,
     getUser,
-    getUsers
+    getUsers,
+    logout
 }
